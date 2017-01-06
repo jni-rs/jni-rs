e97384a2dd727eb0c9fa219518fe43bdaf393f03
Val Vanderschaegen <valere.vanderschaegen@gmail.com>
2017-01-06 10:29:40 -0800

    Add JStaticMethodID and fix static method lookup.

--- src/wrapper/descriptors/method_desc.rs
+++ src/wrapper/descriptors/method_desc.rs
@@ -3,6 +3,7 @@ use errors::*;
 use descriptors::Desc;
 
 use objects::JMethodID;
+use objects::JStaticMethodID;
 use objects::JClass;
 
 use strings::JNIString;
@@ -16,5 +17,15 @@ impl<'a, T, U, V> Desc<'a, JMethodID<'a>> for (T, U, V)
 {
     fn lookup(self, env: &JNIEnv<'a>) -> Result<JMethodID<'a>> {
         env.get_method_id(self.0, self.1, self.2)
+    }
+}
+
+impl<'a, T, U, V> Desc<'a, JStaticMethodID<'a>> for (T, U ,V)
+    where T: Desc<'a, JClass<'a>>,
+          U: Into<JNIString>,
+          V: Into<JNIString>
+{
+    fn lookup(self, env: &JNIEnv<'a>) -> Result<JStaticMethodID<'a>> {
+        env.get_static_method_id(self.0, self.1, self.2)
     }
 }
