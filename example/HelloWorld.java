5dd7ba294e7e766f739c45f5281dc9e1de271825
angel-manuel <angel_manuel_martin@yahoo.es>
2017-07-29 20:12:52 +0200

    Adding callback examples

--- example/HelloWorld.java
+++ example/HelloWorld.java
@@ -1,5 +1,10 @@
 class HelloWorld {
     private static native String hello(String input);
+    private static native void factAndCallMeBack(int n, HelloWorld callback);
+
+    private static native long counterNew(HelloWorld callback);
+    private static native void counterIncrement(long counter_ptr);
+    private static native void counterDestroy(long counter_ptr);
 
     static {
         System.loadLibrary("mylib");
@@ -8,5 +13,23 @@ class HelloWorld {
     public static void main(String[] args) {
         String output = HelloWorld.hello("josh");
         System.out.println(output);
+
+        HelloWorld.factAndCallMeBack(6, new HelloWorld());
+
+        long counter_ptr = counterNew(new HelloWorld());
+
+        for (int i = 0; i < 5; i++) {
+          counterIncrement(counter_ptr);
+        }
+
+        counterDestroy(counter_ptr);
+    }
+
+    public void factCallback(int res) {
+      System.out.println("factCallback: res = " + res);
+    }
+
+    public void counterCallback(int count) {
+      System.out.println("counterCallback: count = " + count);
     }
 }
--- example/mylib/src/lib.rs
+++ example/mylib/src/lib.rs
@@ -1,27 +1,29 @@
 extern crate jni;
 
-// This is the interface to the JVM that we'll call the majority of our methods on.
+// This is the interface to the JVM that we'll
+// call the majority of our methods on.
 use jni::JNIEnv;
 
 // These objects are what you should use as arguments to your native function.
 // They carry extra lifetime information to prevent them escaping this context
 // and getting used after being GC'd.
-use jni::objects::{JClass, JString};
+use jni::objects::{JClass, JString, JObject, GlobalRef};
 
 // This is just a pointer. We'll be returning it from our function.
 // We can't return one of the objects with lifetime information because the
 // lifetime checker won't let us.
-use jni::sys::jstring;
+use jni::sys::{jint, jlong, jstring};
 
 // This keeps rust from "mangling" the name and making it unique for this crate.
 #[no_mangle]
-// This turns off linter warnings because the name doesn't conform to conventions.
+// This turns off linter warnings because
+// the name doesn't conform to conventions.
 #[allow(non_snake_case)]
 pub extern "system" fn Java_HelloWorld_hello(env: JNIEnv,
                                              // this is the class that owns our
-                                             // static method. Not going to be used,
-                                             // but still needs to have an argument
-                                             // slot
+                                             // static method. Not going to be
+                                             // used, but still needs to have
+                                             // an argument slot
                                              class: JClass,
                                              input: JString)
                                              -> jstring {
@@ -37,6 +39,76 @@ pub extern "system" fn Java_HelloWorld_hello(env: JNIEnv,
 
     // Finally, extract the raw pointer to return.
     output.into_inner()
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub extern "system" fn Java_HelloWorld_factAndCallMeBack(env: JNIEnv,
+                                                         _class: JClass,
+                                                         n: jint,
+                                                         callback: JObject) {
+    let i = n as i32;
+    let res: jint = (2..i + 1).product();
+
+    env.call_method(callback, "factCallback", "(I)V", &[res.into()]).unwrap();
+}
+
+struct Counter {
+    count: i32,
+    callback: GlobalRef,
+}
+
+impl Counter {
+    pub fn new(callback: GlobalRef) -> Counter {
+        Counter {
+            count: 0,
+            callback: callback,
+        }
+    }
+
+    pub fn increment(&mut self, env: JNIEnv) {
+        self.count = self.count + 1;
+        env.call_method(*self.callback.as_ref(),
+                         "counterCallback",
+                         "(I)V",
+                         &[self.count.into()])
+            .unwrap();
+    }
+}
+
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub unsafe extern "system" fn Java_HelloWorld_counterNew(env: JNIEnv,
+                                                         _class: JClass,
+                                                         callback: JObject)
+                                                         -> jlong {
+    let global_ref = env.new_global_ref(callback).unwrap();
+    let counter = Counter::new(global_ref);
+
+    Box::into_raw(Box::new(counter)) as jlong
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub unsafe extern "system" fn Java_HelloWorld_counterIncrement(
+    env: JNIEnv,
+    _class: JClass,
+    counter_ptr: jlong
+){
+    let counter = &mut *(counter_ptr as *mut Counter);
+
+    counter.increment(env);
+}
+
+#[no_mangle]
+#[allow(non_snake_case)]
+pub unsafe extern "system" fn Java_HelloWorld_counterDestroy(
+    _env: JNIEnv,
+    _class: JClass,
+    counter_ptr: jlong
+){
+    let _boxed_counter = Box::from_raw(counter_ptr as *mut Counter);
 }
 
 #[cfg(test)]
