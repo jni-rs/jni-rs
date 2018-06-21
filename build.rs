a882499c8adc08fab432973e9ddcd83facfdddb5
Alexander Irbis <irbis.labs@gmail.com>
2018-06-21 17:53:18 +0300

    Fix the platform dependent search for libjvm in system paths.

--- build.rs
+++ build.rs
@@ -1,38 +1,47 @@
 extern crate walkdir;
 
 use std::env;
-use std::fs::symlink_metadata;
+use std::ffi;
+use std::fs;
 use std::path::{
     Path,
     PathBuf,
 };
 
+#[cfg(windows)]
+const LIBJVM_NAME: &str = "jvm.dll";
+#[cfg(all(unix, any(target_os = "macos", target_os = "ios")))]
+const LIBJVM_NAME: &str = "libjvm.dylib";
+#[cfg(all(unix, not(any(target_os = "macos", target_os = "ios"))))]
+const LIBJVM_NAME: &str = "libjvm.so";
+
+#[cfg(windows)]
+const JAVA_EXE_NAME: &str = "java.exe";
+#[cfg(not(windows))]
+const JAVA_EXE_NAME: &str = "java";
+
+#[cfg(windows)]
+const PATHS_SEP: &str = ";";
+#[cfg(not(windows))]
+const PATHS_SEP: &str = ":";
+
 fn main() {
     if cfg!(feature = "invocation") {
-        let libjvm_path = env::var("JAVA_HOME")
-            .ok()
-            .and_then(|p| find_libjvm(p))
-            .or_else(|| find_java_home().and_then(|p| find_libjvm(p)));
+        let libjvm_path = env::var("JAVA_HOME").ok()
+            .and_then(find_libjvm_in_java_home)
+            .or_else(find_java_home)
+            .and_then(find_libjvm_in_java_home)
+            .or_else(find_libjvm_in_library_paths)
+            .unwrap_or_else(|| panic!("Failed to find {}. Try setting JAVA_HOME", LIBJVM_NAME));
 
-        match libjvm_path {
-            Some(path) => println!("cargo:rustc-link-search=native={}", path.display()),
-            None => panic!("Failed to find libjvm.so. Try setting JAVA_HOME"),
-        }
-
+        println!("cargo:rustc-link-search=native={}", libjvm_path.display());
         println!("cargo:rustc-link-lib=dylib=jvm");
     }
 }
 
-fn find_libjvm<S: AsRef<Path>>(path: S) -> Option<PathBuf> {
+fn find_libjvm_in_java_home<S: AsRef<Path>>(path: S) -> Option<PathBuf> {
     let walker = walkdir::WalkDir::new(path).follow_links(true);
-
-    let expected_name = if cfg!(target_os = "windows") {
-        "jvm.dll"
-    } else if cfg!(target_os = "linux") {
-        "libjvm.so"
-    } else {
-        "libjvm.dylib"
-    };
+    let libjvm_name = ffi::OsString::from(LIBJVM_NAME);
 
     for entry in walker {
         let entry = match entry {
@@ -40,9 +49,7 @@ fn find_libjvm<S: AsRef<Path>>(path: S) -> Option<PathBuf> {
             Err(_e) => continue,
         };
 
-        let file_name = entry.file_name().to_str().unwrap_or("");
-
-        if file_name == expected_name {
+        if entry.file_name() == libjvm_name {
             return entry.path().parent().map(Into::into);
         }
     }
@@ -50,39 +57,71 @@ fn find_libjvm<S: AsRef<Path>>(path: S) -> Option<PathBuf> {
     None
 }
 
+#[cfg(windows)]
+fn find_libjvm_in_library_paths() -> Option<PathBuf> {
+    env::var("PATH").ok()
+        .and_then(check_lib_paths)
+}
+
+#[cfg(all(unix, any(target_os = "macos", target_os = "ios")))]
+fn find_libjvm_in_library_paths() -> Option<PathBuf> {
+    env::var("LD_LIBRARY_PATH").ok()
+        .and_then(check_lib_paths)
+        .or_else(|| {
+            env::var("DYLD_LIBRARY_PATH").ok()
+                .and_then(check_lib_paths)
+        })
+}
+
+#[cfg(all(unix, not(any(target_os = "macos", target_os = "ios"))))]
+fn find_libjvm_in_library_paths() -> Option<PathBuf> {
+    env::var("LD_LIBRARY_PATH").ok()
+        .and_then(check_lib_paths)
+}
+
+fn check_lib_paths<P: AsRef<str>>(paths: P) -> Option<PathBuf> {
+    paths.as_ref().split(PATHS_SEP)
+        .filter(|p| is_lib_path(p))
+        .flat_map(follow_symlinks)
+        .next()
+}
+
+fn is_lib_path<P: AsRef<Path>>(path: P) -> bool {
+    path.as_ref().join(LIBJVM_NAME).is_file()
+}
+
 fn find_java_home() -> Option<PathBuf> {
-    let path = match env::var("PATH").ok() {
-        Some(p) => p,
-        None => return None,
-    };
+    env::var("PATH").ok()
+        .and_then(|path_var| {
+            path_var.split(PATHS_SEP)
+                .filter_map(java_home_if_exe_path)
+                .next()
+        })
+}
 
-    let path_sep = if cfg!(target_os = "windows") {
-        ";"
-    } else {
-        ":"
-    };
-
-    let paths = path.split(path_sep);
-    let (mut exe_path, mut exe_meta): (PathBuf, _) = match paths
-        .filter_map(|p| symlink_metadata(p).map(|m| (p.into(), m)).ok())
-        .nth(0)
-    {
-        Some(v) => v,
-        None => return None,
-    };
-
-    while exe_meta.file_type().is_symlink() {
-        match (
-            ::std::fs::read_link(&exe_path).ok(),
-            symlink_metadata(&exe_path).ok(),
-        ) {
-            (Some(p), Some(m)) => {
-                exe_path = p;
-                exe_meta = m;
-            }
-            _ => return None,
-        }
+fn java_home_if_exe_path<P: AsRef<Path>>(path: P) -> Option<PathBuf> {
+    let path = path.as_ref();
+    if !path.join(JAVA_EXE_NAME).is_file() {
+        return None;
     }
+    follow_symlinks(path)
+        .and_then(|path| {
+            path.parent()
+                .and_then(|p| p.parent())
+                .map(Into::into)
+        })
+}
 
-    exe_path.parent().and_then(|p| p.parent()).map(Into::into)
+fn follow_symlinks<P: AsRef<Path>>(path: P) -> Option<PathBuf> {
+    let path = path.as_ref();
+    fs::symlink_metadata(path)
+        .and_then(|mut meta| {
+            let mut path = PathBuf::from(path);
+            while meta.file_type().is_symlink() {
+                meta = fs::symlink_metadata(&path)?;
+                path = fs::read_link(&path)?;
+            };
+            Ok(path)
+        })
+        .ok()
 }
