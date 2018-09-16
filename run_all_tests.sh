e6f122d1a1d67637a875b354313a63d00dfbe16f
Dmitry Timofeev <dmittimofeev@ya.ru>
2018-09-16 13:15:44 +0300

    Add a script running all tests

---
+++ run_all_tests.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+# Runs all Rust and Java tests
+
+# Fail immediately in case of errors and/or unset variables
+set -eu -o pipefail
+
+# Run all Rust tests
+#
+# --all-features is used to enable 'invocation' and 'backtrace' feature in 'jni'
+# crate because passing them directly is not supported:
+# https://github.com/rust-lang/cargo/issues/5364
+cargo test --all-features
+
+# Run Java integration tests. Since benchmarks do not have tests, we skip them.
+mvn verify --projects java-tests --also-make
