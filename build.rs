94e9acb5cc9a11090af90e642c5cc44dd476f636
Ilya Bogdanov <ilya.bogdanov@xdev.re>
2018-10-09 22:38:29 +0300

    Refactor build.rs for proper Windows support.

--- build.rs
+++ build.rs
@@ -1,38 +1,61 @@
 extern crate walkdir;
 
 use std::env;
-use std::fs::symlink_metadata;
 use std::path::{
     Path,
     PathBuf,
 };
+use std::process::Command;
+
+#[cfg(target_os = "windows")]
+const EXPECTED_JVM_FILENAME: &str = "jvm.dll";
+#[cfg(target_os = "linux")]
+const EXPECTED_JVM_FILENAME: &str = "libjvm.so";
+#[cfg(target_os = "macos")]
+const EXPECTED_JVM_FILENAME: &str = "libjvm.dylib";
 
 fn main() {
     if cfg!(feature = "invocation") {
-        let libjvm_path = env::var("JAVA_HOME")
-            .ok()
-            .and_then(|p| find_libjvm(p))
-            .or_else(|| find_java_home().and_then(|p| find_libjvm(p)));
+        let java_home = match env::var("JAVA_HOME") {
+            Ok(java_home) => PathBuf::from(java_home),
+            Err(_) => find_java_home().expect("Failed to find Java home directory. Try setting JAVA_HOME")
+        };
 
-        match libjvm_path {
-            Some(path) => println!("cargo:rustc-link-search=native={}", path.display()),
-            None => panic!("Failed to find libjvm.so. Try setting JAVA_HOME"),
+        let libjvm_path = find_libjvm(&java_home).expect("Failed to find libjvm.so. Check JAVA_HOME");
+
+        println!("cargo:rustc-link-search=native={}", libjvm_path.display());
+
+        if cfg!(windows) {
+            let lib_path = java_home.join("lib");
+            println!("cargo:rustc-link-search={}", lib_path.display());
         }
 
         println!("cargo:rustc-link-lib=dylib=jvm");
     }
 }
 
+fn find_java_home() -> Option<PathBuf> {
+    Command::new("java")
+        .arg("-XshowSettings:properties")
+        .arg("-version")
+        .output()
+        .ok()
+        .and_then(|output| {
+            let stdout = String::from_utf8_lossy(&output.stdout);
+            let stderr = String::from_utf8_lossy(&output.stderr);
+            for line in stdout.lines().chain(stderr.lines()) {
+                if line.contains("java.home") {
+                    let pos = line.find('=').unwrap() + 1;
+                    let path = line[pos..].trim();
+                    return Some(PathBuf::from(path));
+                }
+            }
+            None
+        })
+}
+
 fn find_libjvm<S: AsRef<Path>>(path: S) -> Option<PathBuf> {
     let walker = walkdir::WalkDir::new(path).follow_links(true);
-
-    let expected_name = if cfg!(target_os = "windows") {
-        "jvm.dll"
-    } else if cfg!(target_os = "linux") {
-        "libjvm.so"
-    } else {
-        "libjvm.dylib"
-    };
 
     for entry in walker {
         let entry = match entry {
@@ -42,47 +65,10 @@ fn find_libjvm<S: AsRef<Path>>(path: S) -> Option<PathBuf> {
 
         let file_name = entry.file_name().to_str().unwrap_or("");
 
-        if file_name == expected_name {
+        if file_name == EXPECTED_JVM_FILENAME {
             return entry.path().parent().map(Into::into);
         }
     }
 
     None
-}
-
-fn find_java_home() -> Option<PathBuf> {
-    let path = match env::var("PATH").ok() {
-        Some(p) => p,
-        None => return None,
-    };
-
-    let path_sep = if cfg!(target_os = "windows") {
-        ";"
-    } else {
-        ":"
-    };
-
-    let paths = path.split(path_sep);
-    let (mut exe_path, mut exe_meta): (PathBuf, _) = match paths
-        .filter_map(|p| symlink_metadata(p).map(|m| (p.into(), m)).ok())
-        .nth(0)
-    {
-        Some(v) => v,
-        None => return None,
-    };
-
-    while exe_meta.file_type().is_symlink() {
-        match (
-            ::std::fs::read_link(&exe_path).ok(),
-            symlink_metadata(&exe_path).ok(),
-        ) {
-            (Some(p), Some(m)) => {
-                exe_path = p;
-                exe_meta = m;
-            }
-            _ => return None,
-        }
-    }
-
-    exe_path.parent().and_then(|p| p.parent()).map(Into::into)
 }
