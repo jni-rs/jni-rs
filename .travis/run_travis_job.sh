a2be2c8cb9ad7d4b0dcf039e1d7733873b2b9bee
Sergey Kletsun <sergey.kletsun@gmail.com>
2019-06-14 09:19:18 +0300

    Updated travis build configuration. Fixed clippy for nightly toolchain. Removed `cargo audit`.

--- .travis/run_travis_job.sh
+++ .travis/run_travis_job.sh
@@ -6,24 +6,35 @@ set -eu -o pipefail
 # Echo commands so that the progress can be seen in CI server logs.
 set -x
 
-# Install cargo-audit if it's not already.
-cargo audit --version || cargo install cargo-audit --force
+# Install clippy
+if rustc --version | grep -q "nightly"
+then
+    # Install nightly clippy
+    rustup component add clippy --toolchain=nightly || cargo install --git https://github.com/rust-lang/rust-clippy/ --force clippy
+else
+    # Install stable clippy
+    rustup component add clippy
+fi
+cargo clippy -V
 
-# Install clippy and rustfmt.
-rustup component add clippy
+# Install rustfmt
 rustup component add rustfmt
 rustfmt -V
-cargo clippy -V
 
 echo 'Performing checks over the rust code'
 # Check the formatting.
 cargo fmt --all -- --check
 
+# Detect installed Java
+export JAVA_HOME="$(java -XshowSettings:properties -version \
+    2>&1 > /dev/null |\
+    grep 'java.home' |\
+    awk '{print $3}')"
+
 # Run clippy static analysis.
 cargo clippy --all --tests --all-features -- -D warnings
 
-# Run audit of vulnerable dependencies.
-cargo audit
-
 # Run all tests
+LIBJVM_PATH="$(find -L ${JAVA_HOME} -type f -name libjvm.* | xargs -n1 dirname)"
+export LD_LIBRARY_PATH="${LIBJVM_PATH}"
 cargo test --features=backtrace,invocation
