8e73da691de6921e6585024fca5d24f1a91b117f
Sergey Kletsun <sergey.kletsun@gmail.com>
2019-11-07 10:52:18 +0200

    Reworded method comment [ci skip].

--- src/wrapper/strings/ffi_str.rs
+++ src/wrapper/strings/ffi_str.rs
@@ -82,7 +82,8 @@ impl JNIStr {
     ///
     /// # Safety
     ///
-    /// Expects a valid pointer to a valid immutable C string.
+    /// Expects a valid pointer to a null-terminated C string and does not perform any lifetime
+    /// checks for the resulting value.
     pub unsafe fn from_ptr<'a>(ptr: *const c_char) -> &'a JNIStr {
         &*(ffi::CStr::from_ptr(ptr) as *const ffi::CStr as *const ffi_str::JNIStr)
     }
