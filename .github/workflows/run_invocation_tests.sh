aa7551eb8de2a398bd69e2e1933e02a8f37286bc
Stanislav Tkach <stanislav.tkach@gmail.com>
2020-11-01 17:32:53 +0200

    Add shellcheck

--- .github/workflows/run_invocation_tests.sh
+++ .github/workflows/run_invocation_tests.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Fail immediately in case of errors and/or unset variables
+set -eu -o pipefail
+
+# Echo commands so that the progress can be seen in CI server logs.
+set -x
+
+# Check the shell scripts
+shellcheck .github/workflows/run_invocation_tests.sh
+shellcheck test_profile
+
 # Setup LD_LIBRARY_PATH for ITs
 # shellcheck source=/dev/null
 source test_profile
