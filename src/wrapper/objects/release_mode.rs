9e03791aaaba1407bd2e89271d4352213e1a71cf
Dmitry Timofeev <dmitry-timofeev@users.noreply.github.com>
2021-02-06 18:29:22 +0200

    Improve the docs: (#300)
    Remove some redundant info in the docs.
    
    Fix some doc warnings.

--- src/wrapper/objects/release_mode.rs
+++ src/wrapper/objects/release_mode.rs
@@ -7,7 +7,8 @@ use crate::sys::JNI_ABORT;
 #[derive(Clone, Copy)]
 #[repr(i32)]
 pub enum ReleaseMode {
-    /// Copy back the content and free the elems buffer.
+    /// Copy back the content and free the elems buffer. For read-only access, prefer
+    /// [`NoCopyBack`](ReleaseMode::NoCopyBack).
     CopyBack = 0,
     /// Free the buffer without copying back the possible changes.
     NoCopyBack = JNI_ABORT,
