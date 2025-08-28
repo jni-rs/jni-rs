6b190f119cc9579ae9254fc07709a9fcf3937678
Robert Bragg <robert@sixbynine.org>
2025-08-28 22:59:36 +0100

    Remove special-case Desc<JClass> impl for GlobalRef
    Now that GlobalRef no longer erases the reference type that it wraps we
    can have a blanket `Desc<T>` implementation for `GlobalRef<T>`, like we
    do for `AutoLocal`. This avoids the `unsafe` `transmute` that was required
    while `GlobalRef` erased its inner reference type.

--- src/wrapper/descriptors/class_desc.rs
+++ src/wrapper/descriptors/class_desc.rs
@@ -2,7 +2,7 @@ use crate::{
     descriptors::Desc,
     env::JNIEnv,
     errors::*,
-    objects::{AutoLocal, GlobalRef, IntoAutoLocal as _, JClass, JObject},
+    objects::{AutoLocal, IntoAutoLocal as _, JClass},
     strings::JNIString,
 };
 
@@ -17,29 +17,6 @@ where
     }
 }
 
-// Note: We don't implement `Desc<JClass>` for `&JObject` as a transmute like for `GlobalRef`
-//
-// Considering that the APIs that return a class return a `JClass` it shouldn't
-// usually be necessary unless the `JClass` got type erased (like with GlobalRef)
-//
-// Implementing `Desc<JClass>` for `&JObject` as a simple cast would also make
-// it a lot easier to mistakenly pass an object instance in places where a class
-// is required.
-
-// This conversion assumes that the `GlobalRef` is a pointer to a class object.
-
-// TODO: Generify `GlobalRef` and get rid of this `impl`. The transmute is
-// sound-ish at the moment (`JClass` is currently `repr(transparent)`
-// around `JObject`), but that may change in the future. Moreover, this
-// doesn't check if the global reference actually refers to a
-// `java.lang.Class` object.
-unsafe impl<'local, 'obj_ref> Desc<'local, JClass<'static>>
-    for &'obj_ref GlobalRef<JClass<'static>>
-{
-    type Output = &'obj_ref JClass<'static>;
-
-    fn lookup(self, _: &mut JNIEnv<'local>) -> Result<Self::Output> {
-        let obj: &JObject<'static> = self.as_ref();
-        Ok(unsafe { std::mem::transmute::<&JObject<'_>, &JClass<'_>>(obj) })
-    }
-}
+// Note: we don't implement `Desc<JClass>` for `&JObject` as a simple cast would
+// make it a lot easier to mistakenly pass an object instance in places where a
+// class is required.
