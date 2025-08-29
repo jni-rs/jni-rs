f589b79fec3cf3f3f921c9c5b4da24e65ab64045
Robert Bragg <robert@sixbynine.org>
2025-08-29 15:28:49 +0100

    Adds test for AttachConfig

---
+++ tests/threads_attach_config.rs
@@ -0,0 +1,45 @@
+#![cfg(feature = "invocation")]
+
+use std::thread::spawn;
+
+use jni::{objects::JString, AttachConfig};
+
+mod util;
+use util::jvm;
+
+// Tests that an `AttachConfig` can be used to configure the name of the JVM `Thread`
+#[test]
+fn attach_config() {
+    let jvm = jvm();
+
+    let thread = spawn({
+        move || {
+            jvm.attach_current_thread_with_config(
+                || AttachConfig::new().name("test-thread"),
+                None,
+                |env| -> jni::errors::Result<_> {
+                    // Get the current Thread and query the name
+                    let thread = env
+                        .call_static_method(
+                            "java/lang/Thread",
+                            "currentThread",
+                            "()Ljava/lang/Thread;",
+                            &[],
+                        )
+                        .unwrap()
+                        .l()
+                        .unwrap();
+                    let name: JString = env
+                        .call_method(thread, "getName", "()Ljava/lang/String;", &[])?
+                        .l()?
+                        .into();
+                    let name = env.get_string(&name)?;
+                    assert_eq!(name.as_cstr(), c"test-thread");
+                    Ok(())
+                },
+            )
+        }
+    });
+
+    let _ = thread.join().unwrap();
+}
