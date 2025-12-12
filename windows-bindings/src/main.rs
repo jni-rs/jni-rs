fcfee870c3a250093f605ce83d20b35178b8a226
Robert Bragg <robert@sixbynine.org>
2025-12-12 00:03:22 +0000

    Update to windows-bindgen 0.65 (supports 2024 edition)
    This adds a windows-link 0.2 dep since the generated code depends on
    windows-link, even with `--no-deps`

--- windows-bindings/src/main.rs
+++ windows-bindings/src/main.rs
@@ -1,40 +1,9 @@
-// XXX: Once the `windows-link` releases it would be good to use.
-// This slightly diverges from the upstream because of a current `jni` MSRV of 1.77 and because
-// the name `link` is too ambiguous for Rust to allow re-exporting.
-const LINKAGE: &[u8] = br#"
-// jni-rs specific additions vendored from https://github.com/microsoft/windows-rs/blob/master/crates/libs/link/src/lib.rs
-mod windows_targets {
-    #[cfg(all(windows, target_arch = "x86"))]
-    macro_rules! win_link {
-        ($library:literal $abi:literal $($link_name:literal)? fn $($function:tt)*) => (
-            #[link(name = $library, kind = "raw-dylib", modifiers = "+verbatim", import_name_type = "undecorated")]
-            extern $abi {
-                $(#[link_name=$link_name])?
-                pub fn $($function)*;
-            }
-        )
-    }
-    #[cfg(all(windows, not(target_arch = "x86")))]
-    macro_rules! win_link {
-        ($library:literal $abi:literal $($link_name:literal)? fn $($function:tt)*) => (
-            #[link(name = $library, kind = "raw-dylib", modifiers = "+verbatim")]
-            extern $abi {
-                $(#[link_name=$link_name])?
-                pub fn $($function)*;
-            }
-        )
-    }
-    pub(super) use win_link as link;
-}
-"#;
-
-use std::io::Write;
 fn main() {
-    let out_file = "../src/wrapper/windows_sys.rs";
-    windows_bindgen::bindgen(&["--out", out_file, "--etc", "./bindings.config"]);
-    let mut bindings = std::fs::File::options()
-        .append(true)
-        .open(out_file)
-        .unwrap();
-    bindings.write_all(LINKAGE).unwrap();
+    windows_bindgen::bindgen(&[
+        "--out",
+        "../jni/src/windows_sys.rs",
+        "--etc",
+        "./bindings.config",
+    ])
+    .unwrap();
 }
